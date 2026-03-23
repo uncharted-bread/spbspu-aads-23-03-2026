44bc8c9df263a456fac2a5f7cb65edea008cbf89
Michael <gordeychikmika@gmail.com>
2026-03-23 10:56:14 +0300

    tests mini-framework

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
+#include <iostream>
+#include <iomanip>
 #include "vector-top-it.hpp"
+bool testDefaultVector()
+{
+  using topit::Vector;
+  Vector<int> v;
+  return v.isEmpty();
+}
+
 int main()
 {
-  using namespace topit{
-    Vector< int > v1;
-    Vector< int > v2;
-    if (v1 == v2)
-    {
-      //
-    }
+  using test_t = bool(*)();
+  bool (*tests[])() = {
+    testDefaultVector
+  };
+  size_t testCount = sizeof(tests) / sizeof(test_t);
+  for (size_t i = 0; i < testCount; ++i)
+  {
+    std::cout << tests[i]() << ": " << i << "\n";
   }
 }
--- vector-top-it.hpp
+++ vector-top-it.hpp
@@ -1,14 +1,20 @@
 #ifndef VECTOR_TOP_IT_HPP
 #define VECTOR_TOP_IT_HPP
+#include <cstddef>
 namespace topit
 {
   template< class T>
   struct Vector
   {
-    T* data;
-    size_t size capacity;
+    Vector();
+    ~Vector();
+
+    bool isEmpty() const noexcept;
+    private:
+      T* data;
+      size_t size,  capacity;
   };
   template< class T >
-  bool operator == (const Vector<T>& a, const Vector<T>& b);
+  bool operator == (const Vector<T>& lhs, const Vector<T>& rhs);
 }
 #endif
