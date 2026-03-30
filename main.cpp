74f3eaa98ebf8b0fd2b6cd1651316c8d1702063b
Michael <gordeychikmika@gmail.com>
2026-03-30 12:25:58 +0300

    Last practice and HW complete

--- main.cpp
+++ main.cpp
@@ -1,23 +1,105 @@
+#include <ios>
 #include <iostream>
-#include <iomanip>
 #include "vector-top-it.hpp"
-bool testDefaultVector()
+#include <cstddef>
+#include <stdexcept>
+#include <utility>
+
+using topit::Vector;
+bool test1()
 {
-  using topit::Vector;
-  Vector<int> v;
-  return v.isEmpty();
+    Vector< int > v;
+    return v.isEmpty();
+}
+bool test2()
+{
+    Vector<int> v;
+    return v.getSize() == 0;
+}
+bool test3()
+{
+    constexpr size_t size = 3ull;
+    Vector<int> v(size, 0);
+    return v.getSize() == size && v.getCapacity() == size;
+}
+bool test4()
+{
+    constexpr size_t size = 3ull;
+    Vector<int> v(size, 1);
+    try {
+        int& value = v.at(0);
+        int& value1 = v[0];
+        return value == 1 and value1 == 1;
+    } catch(...) {
+        return false;
+    }
+}
+bool test5()
+{
+    constexpr size_t size = 3ull;
+    Vector<int> v(size, 0);
+    try {
+        v.at(size + 1);
+        return false;
+    } catch(const std::out_of_range&) {
+        return true;
+    } catch(...) {
+        return false;
+    }
+}
+bool test6()
+{
+    constexpr size_t size = 3ull;
+    const Vector<int> v(size, 1);
+    try {
+        const int& value = v.at(0);
+        const int& value1 = v[0];
+        return value == 1 and value1 == 1;
+    } catch(...) {
+        return false;
+    }
+}
+bool test7()
+{
+    constexpr size_t size = 3ull;
+    const Vector<int> v(size, 0);
+    try {
+        v.at(size + 1);
+        return false;
+    } catch(const std::out_of_range&) {
+        return true;
+    } catch(...) {
+        return false;
+    }
 }
 
 int main()
 {
-  using test_t = bool(*)();
-  bool (*tests[])() = {
-    testDefaultVector
-  };
-  size_t testCount = sizeof(tests) / sizeof(test_t);
-  std::cout << std::boolalpha;
-  for (size_t i = 0; i < testCount; ++i)
-  {
-    std::cout << tests[i]() << ": " << i << "\n";
-  }
+    using test_t = bool(*)();
+    using case_t = std::pair<test_t, const char*>;
+    case_t tests[] = {
+        {test1, "Default constructor vector must be empty"},
+        {test2, "Default vector must has zero size"},
+        {test3, "Vector constructor with size has non-zero size and capacity"},
+        {test4, "In range access does not generate exceptions"},
+        {test5, "Out of range access generates std::out_of_ranges exceptions"},
+        {test6, "In range access for const Vector does not generate exceptions"},
+        {test7, "Out of range access for const Vector generates std::out_of_ranges exceptions"}
+    };
+    size_t count = sizeof(tests) / sizeof(case_t);
+    std::cout << std::boolalpha;
+    bool result = true;
+    size_t successes = 0, fails = 0;
+    for(size_t i = 0; i < count; ++i) {
+        bool case_result = tests[i].first();
+        successes += case_result;
+        fails += !case_result;
+        result = result && case_result;
+        std::cout << case_result << ": " << tests[i].second << "\n";
+    }
+    std::cout << "SUMMARY\n";
+    std::cout << result << ": TEST_RESULT\n";
+    std::cout << fails << ": failed_tests\n";
+    std::cout << successes << ": successes_tests\n";
+    return 0;
 }
--- vector-top-it.hpp
+++ vector-top-it.hpp
@@ -1,41 +1,127 @@
 #ifndef VECTOR_TOP_IT_HPP
 #define VECTOR_TOP_IT_HPP
+
 #include <cstddef>
+#include <stdexcept>
 namespace topit
 {
-  template< class T>
-  struct Vector
-  {
-    Vector();
-    ~Vector();
+    template < class T >
+    struct Vector
+    {
+      Vector();
+      Vector(size_t size, const T& val);
+      ~Vector();
+      Vector<T>& operator=(const Vector<T>&) = delete;
+      bool isEmpty() const noexcept;
+      size_t getSize() const noexcept;
+      size_t getCapacity() const noexcept;
 
-    Vector(const Vector< T >&) = delete;
-    Vector< T >& operator=(const Vector< T >&) = delete;
+      void pushBack(const T& val);
 
-    bool isEmpty() const noexcept;
+      T& operator[](size_t id) noexcept;
+      const T& operator[](size_t id) const noexcept;
+      T& at(size_t id);
+      const T& at(size_t id) const;
+
     private:
       T* data_;
-      size_t size_,  capacity_;
-  };
-  template< class T >
-  bool operator == (const Vector<T>& lhs, const Vector<T>& rhs);
+      size_t size_, capacity_;
+      explicit Vector(size_t size);
+    };
+    template<class T>
+    bool operator==(const Vector<T>& v1, const Vector<T>& v2);
+    template<class T>
+    bool operator!=(const Vector<T>& v1, const Vector<T>& v2);
 }
-
-template <class T>
-bool topit::Vector<T>::isEmpty() const noexcept
-{
-  return false;
-}
-
-template <class T>
-topit::Vector<T>::Vector():
-  data_(nullptr),
-  size_(0),
-  capacity_(0)
+template < class T >
+topit::Vector<T>::Vector(): data_(nullptr), size_(0), capacity_(0)
 {}
-template <class T>
+template < class T >
 topit::Vector<T>::~Vector()
 {
-  delete[] data_;
+  delete [] data_;
+}
+template< class T >
+bool topit::Vector<T>::isEmpty() const noexcept
+{
+  return !size_;
+}
+template<class T>
+size_t topit::Vector<T>::getSize() const noexcept
+{
+  return size_;
+}
+template<class T>
+topit::Vector<T>::Vector(size_t size):
+ data_(size ? new T[size] : nullptr),
+ size_(size),
+ capacity_(size)
+{}
+template<class T>
+T& topit::Vector<T>::at(size_t id)
+{
+  return const_cast< T& >(static_cast< const Vector< T >* >(this) -> at(id));
+}
+template<class T>
+const T& topit::Vector<T>::at(size_t id) const
+{
+    if (id < getSize()) {
+        return data_[id];
+    }
+  throw std::out_of_range("bad id");
+}
+template<class T>
+topit::Vector<T>::Vector(size_t size, const T& val):
+ Vector(size)
+{
+  for (size_t i = 0; i < size; i++) {
+    data_[i] = val;
+  }
+}
+template<class T>
+size_t topit::Vector<T>::getCapacity() const noexcept
+{
+  return capacity_;
+}
+template<class T>
+void topit::Vector<T>::pushBack(const T& val)
+{
+  if (size_ >= capacity_) {
+    size_t newCap = (capacity_ == 0) ? 1 : capacity_ * 2;
+	T* newData = new T[newCap];
+    for (size_t i = 0; i < size_; ++i) {
+      newData[i] = data_[i];
+    }
+    delete[] data_;
+    data_ = newData;
+    capacity_ = newCap;
+  }
+  data_[size_++] = val;
+}
+template<class T>
+T& topit::Vector<T>::operator[](size_t id) noexcept
+{
+  return data_[id];
+}
+template<class T>
+const T& topit::Vector<T>::operator[](size_t id) const noexcept
+{
+  return data_[id];
+}
+
+template<class T>
+bool topit::operator==(const Vector<T>& v1, const Vector<T>& v2) {
+    if (v1.getSize() != v2.getSize())
+        return false;
+    for (size_t i = 0; i < v1.getSize(); ++i) {
+        if (v1[i] != v2[i])
+            return false;
+    }
+    return true;
+}
+
+template<class T>
+bool topit::operator!=(const Vector<T>& v1, const Vector<T>& v2) {
+    return !(v1 == v2);
 }
 #endif
